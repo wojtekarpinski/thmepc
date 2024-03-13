3f0c2c6a76c6e3b2a6438f48df6e240ab9a7715b
Wojtek Karpiński <wa.karpinski@uw.edu.pl>
2024-03-13 11:40:13 +0100

    Search form looks and workings

--- scripts/searchFormHandler.js
+++ scripts/searchFormHandler.js
@@ -1,3 +1,36 @@
-function searchFormHandler() {
-    
+Drupal.behaviors.searchState = 
+{
+    attach: function readSearchState()
+    {
+        
+            if(document.body.classList.contains("path-szukaj")) {
+                let formItems = document.getElementsByClassName("form-item");
+                let itemsNb = formItems.length;
+                let tempState = localStorage.searchState;
+                let searchSwitch = document.getElementById("search-selector");
+                console.log("initial searchState:");
+                console.log(tempState);
+                if(typeof tempState === 'undefined') 
+                {
+                    localStorage.searchState = 'basic';
+                }
+                if(localStorage.searchState === 'basic') {
+                    //Display basic search field
+                    formItems[0].style.display = "block";
+                    for(let i = 1; i < itemsNb; i++) {
+                        formItems[i].style.display = "";
+                    }
+                    
+                } else {
+                    //Display advanced search fields
+                    for(let i = 1; i < itemsNb; i++) {
+                        formItems[i].style.display = "block";
+                    }
+                    formItems[0].style.display = "";
+                    searchSwitch.innerHTML = "Proste wyszukiwanie <span class=\"icon-circle-up\"></span>";
+                }
+
+            }
+            
+        }
 }
